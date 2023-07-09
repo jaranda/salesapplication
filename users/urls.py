7836ae82a6fffe04e50b4db235fd9cede93468cc
Juan Aranda-Alvarez <jaranda@a3nstudio.com>
2023-07-09 00:03:05 -0500

    Creating Users model and its endpoint (GET). - Migration created and run.

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from users.views import hello
+from users.views import users
 
 urlpatterns = [
-    path('hello/', hello),
+    path('users/', users),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import User
+
 
 @api_view(['GET'])
-def hello(request):
-    return Response('hello')
+def users(request):
+    users = User.objects.all()
+    return Response(users)
