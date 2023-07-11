db986b74c1212bf3b54749caf167e69fa7ad9000
Juan Aranda-Alvarez <jaranda@a3nstudio.com>
2023-07-11 16:38:27 -0500

    User Serializer and Register View function. - Changed the email from CharField to EmailField.

--- users/models.py
+++ users/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class User(models.Model):
     first_name: models.CharField(max_length=200)
     last_name: models.CharField(max_length=200)
-    email = models.CharField(max_length=200, unique=True)
+    email: models.EmailField(max_length=200, unique=True)
     password = models.CharField(max_length=200)
