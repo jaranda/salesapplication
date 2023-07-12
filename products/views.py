54902221464dc6b4e106695f45553c8d51fba6c4
Juan Aranda-Alvarez <jaranda@a3nstudio.com>
2023-07-12 14:34:24 -0500

    Products endpoint - Create - Get list - Updated by ID - Delete by ID - Upload image

--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
+from django.core.files.storage import default_storage
 from rest_framework import generics, mixins
+from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from config.pagination import CustomPagination
 from products.models import Product
@@ -38,3 +41,16 @@ class ProductGenericAPIView(
     def delete(self, request, pk=None):
         return self.destroy(request, pk)
 
+class FileUploadView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    parser_classes = (MultiPartParser, )
+    
+    def post(self, request):
+        file = request.FILES['image']
+        file_name = default_storage.save(file.name, file)
+        url = default_storage.url(file_name)
+
+        return Response({
+            'url': url
+        })
