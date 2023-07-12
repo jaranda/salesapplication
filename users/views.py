51b7adad92660ccb0c90a6c51da52ee8807d93d6
Juan Aranda-Alvarez <jaranda@a3nstudio.com>
2023-07-12 12:33:56 -0500

    sends  'permissions' list of human readable roles

--- users/views.py
+++ users/views.py
@@ -51,10 +51,10 @@ class AuthenticatedUser(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        serializer = UserSerializer(request.user)
-
+        data = UserSerializer(request.user).data
+        data['permissions'] = [p['name'] for p in data['role']['permissions']]
         return Response({
-            'data': serializer.data
+            'data': data
         })
 
 @api_view(['POST'])
