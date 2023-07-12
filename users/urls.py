197200ba8acd57cbc2e2e2bfe840a69e8482bcf7
Juan Aranda-Alvarez <jaranda@a3nstudio.com>
2023-07-11 22:59:23 -0500

    Creating the logout endpoint - we delete the cookie here.

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from users.views import AuthenticatedUser, login, register, users
+from users.views import AuthenticatedUser, login, logout, register, users
 
 urlpatterns = [
     path('users/', users),
     path('register/', register),
     path('login/', login),
-    path('user/', AuthenticatedUser.as_view())
+    path('user/', AuthenticatedUser.as_view()),
+    path('logout/', logout)
 ]
