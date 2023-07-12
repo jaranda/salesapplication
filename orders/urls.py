45ffb65c36a848950e3cec7dc29ef34dc10b8057
Juan Aranda-Alvarez <jaranda@a3nstudio.com>
2023-07-12 16:33:26 -0500

    Exporting CSV file of the Orders.

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import OrderGenericAPIView
+from .views import ExportAPIView, OrderGenericAPIView
 
 urlpatterns = [
    path('orders/', OrderGenericAPIView.as_view()),
-   path('orders/<str:pk>', OrderGenericAPIView.as_view())
+   path('orders/<str:pk>', OrderGenericAPIView.as_view()),
+   path('export/', ExportAPIView.as_view()),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,13 @@
+import csv
+
+from django.http import HttpResponse
 from rest_framework import generics, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from config.pagination import CustomPagination
-from orders.models import Order
+from orders.models import Order, OrderItem
 from orders.serializers import OrderSerializer
 from users.authentication import JWTAuthentication
 
@@ -21,4 +25,26 @@ class OrderGenericAPIView( generics.GenericAPIView, mixins.ListModelMixin, mixin
                 'data': self.retrieve(request, pk).data
             })
 
-        return self.list(request)    
+        return self.list(request)
+
+class ExportAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=orders.csv'
+
+        orders = Order.objects.all()
+        writer = csv.writer(response)
+
+        writer.writerow(['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity'])
+
+        for order in orders:
+            writer.writerow([order.id, order.name, order.email, '', '', ''])
+            orderItems = OrderItem.objects.all().filter(order_id=order.id)
+
+            for item in orderItems:
+                writer.writerow(['', '', '', item.product_title, item.price, item.quantity])
+
+        return response
