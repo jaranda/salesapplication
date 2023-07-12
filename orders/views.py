d8f3b6acf98982f01b69633b68493cfafc641d85
Juan Aranda-Alvarez <jaranda@a3nstudio.com>
2023-07-12 15:35:18 -0500

    From ProductsGenericAPIView to OrderGenericAPIView

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from config.pagination import CustomPagination
+from products.models import Product
+from products.serializers import ProductSerializer
+from users.authentication import JWTAuthentication
+
+
+class OrderGenericAPIView( generics.GenericAPIView, mixins.ListModelMixinn ):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = CustomPagination
+
+    def get(self, request, pk=None):
+        if pk:
+            return Response({
+                'data': self.retrieve(request, pk).data
+            })
+
+        return self.list(request)    
